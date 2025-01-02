bd581721ad3c59660eafeaff726bb92dcc9af361
Andre <andrezorek@gmail.com>
2025-01-02 15:10:16 -0300

    spring started

---
+++ src/test/java/com/andrezorek/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrezorek.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
