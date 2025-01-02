f15768f7fffa24f645ccc284a136c1ec8ac108bd
Andre <andrezorek@gmail.com>
2025-01-02 16:15:28 -0300

    add dotenv e conexao com postgresdb via springjpa. env vars seguras e requisição a api realizada com sucesso

--- src/main/java/com/andrezorek/literalura/LiteraluraApplication.java
+++ src/main/java/com/andrezorek/literalura/LiteraluraApplication.java
@@ -1,13 +1,33 @@
 package com.andrezorek.literalura;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static services.RequestHandler.fetch;
+
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		// Carrega as variáveis do .env e seta como propriedades do sistema
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// hello world
+		System.out.println("Hello from Springland");
+
+		var res = fetch("https://gutendex.com/books/?ids=3333");
+
+		System.out.println("res = " + res);
+
+	}
 }
